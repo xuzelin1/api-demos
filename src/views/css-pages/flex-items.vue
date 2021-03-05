954b82c0754916c91787aea527a6dc39cdd9fa20
zelin.xu <zelin.xu@shopee.com>
2021-03-05 21:20:51 +0800

    fix: 调整

--- src/views/css-pages/flex-items.vue
+++ src/views/css-pages/flex-items.vue
@@ -7,16 +7,27 @@
         </div>
       </transition-group>
     </div>
+    <hr />
+
+    <div class="flex-page-item flex-page-item-second">
+      <div class="main">
+        <span class="change-btn" @click="sideShow = !sideShow">收起/展开</span>
+      </div>
+      <transition name="side">
+        <div class="side" v-show="sideShow"></div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from "@vue/runtime-core";
+import { defineComponent, reactive, ref } from "@vue/runtime-core";
 
 export default defineComponent({
   name: 'Flex',
   setup() {
     const list = reactive([1, 2, 3, 4]);
+    const sideShow = ref(true);
 
     const removeItem = (index: number) => {
       list.splice(index, 1);
@@ -25,6 +36,7 @@ export default defineComponent({
     return {
       list,
       removeItem,
+      sideShow,
     }
   }
 })
@@ -54,14 +66,42 @@ export default defineComponent({
     transition: all 1.5s ease-in-out;
   }
   .list-enter, .list-leave-to {
-    // opacity: 0;
-    // transform: translateY(30px);
     flex: 0.0001 !important;
   }
   .list-enter-to, .list-leave {
-    // opacity: 0;
-    // transform: translateY(30px);
     flex: 2;
+  }
+}
+
+.flex-page-item-second {
+  display: flex;
+  height: 300px;
+  width: 100%;
+
+  .main {
+    flex-grow: 2;
+    background-color: royalblue;
+
+    .change-btn {
+      color: #fff;
+      background-color: skyblue;
+    }
+  }
+  .side {
+    flex-grow: 1;
+    background-color: sandybrown;
+  }
+
+  .side-enter-active, .side-leave-active {
+    transition: flex-grow 1.5s ease-in-out;
+  }
+  .side-enter, .side-leave-to {
+    width: 0 !important;
+    flex-grow: 0.00001 !important;
+  }
+  .side-enter-to, .side-leave {
+    width: 100% !important;
+    flex-grow: 1 !important;
   }
 }
 </style>
